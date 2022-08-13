c9c12c0bf8b4ebe47d36653954269cdb42c0c214
YY Ong <yuanyng@outlook.com>
2022-08-13 16:25:30 +0800

    corrected properties db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_072250) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_082005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "properties", force: :cascade do |t|
     t.string "address"
-    t.string "type"
+    t.string "category"
     t.boolean "has_tv"
     t.boolean "has_kitchen"
     t.boolean "has_aircon"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_072250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "name"
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
